7a6bc5ffb8605356a5b1663b24ff3d7339c01b87
Komputronika <piepin@skkkbandung.or.id>
2018-06-05 11:43:53 +0700

    Upload perdana

---
+++ BlinkMillis.h
@@ -0,0 +1,81 @@
+//------------------------------------------------
+//
+//  Library Arduino: Blink Millis
+//  Blink menggunakan millis()
+//  - Tidak memblok eksekusi
+//  - Dapat membuat banyak blink secara bersamaan
+//  - Jumlah perulangan dapat diatur
+//  - Dapat membuat blink infinity
+//
+//  Author: Komputronika.com
+//
+//------------------------------------------------
+
+#include "Arduino.h"
+
+#ifndef BLINKMILLIS_H
+#define BLINKMILLIS_H
+
+class Blinker {
+
+    protected:
+        int pinLED;                 // Pin untuk LED
+        long waktuOn;               // Waktu menyala
+        long waktuOff;              // Waktu mati
+        int jumlahUlang;            // Jumlah blink diulang
+        int statusLED;              // Status LED terakhir
+        unsigned long millisLalu;   // Mencatat waktu terakhi LED diupdate
+
+    public:
+
+        //---------------------------------
+        // Constructor: Blinker
+        //---------------------------------
+        Blinker(int pin, long on, long off, int ulang = -1)  {
+            pinLED = pin;
+            pinMode(pinLED, OUTPUT);
+
+            waktuOn = on;
+            waktuOff = off;
+
+            jumlahUlang = ulang;
+            statusLED = LOW;
+            millisLalu = 0;
+        }
+
+        //---------------------------------
+        // Run: Jalankan LED kedap-kedip
+        //---------------------------------
+        void Run() {
+            // Catat nilai millis saat ini
+            unsigned long millisKini = millis();
+
+            // if (jumlahUlang>0 or jumlahUlang==-1) {
+
+            // Ulangi selama jumlah jumlahUlang tidak sama dengan 0 atau jumlahUlang == -1 (infinity)
+            if (jumlahUlang != 0) {
+                if((statusLED == HIGH) && (millisKini - millisLalu >= waktuOn)) {
+                    statusLED = LOW;                    // Jadikan off
+                    millisLalu = millisKini;            // Catat lagi waktunya
+                    digitalWrite(pinLED, statusLED);    // Update kondisi LED
+                    if (jumlahUlang>0) jumlahUlang--;   // Kurangi jumlah perulangan
+                } else if ((statusLED == LOW) && (millisKini - millisLalu >= waktuOff)) {
+                    statusLED = HIGH;                   // Jadikan on
+                    millisLalu = millisKini;            // Catat waktunya
+                    digitalWrite(pinLED, statusLED);    // Update kondisi LED
+                }
+            }
+        }
+
+        //---------------------------------
+        // Stop: Stop LED blink
+        //---------------------------------
+        void Stop() {
+            jumlahUlang = 0;                    // Habiskan jumlahUlang nya
+            statusLED = LOW;                    // Status LED jadi LOW
+            digitalWrite(pinLED, statusLED);    // Update kondisi LED
+        }
+
+}; // Akhir Class Blinker
+
+#endif
---
+++ examples/TestBlink/TestBlink.ino
@@ -0,0 +1,16 @@
+// Include header
+#include "BlinkMillis.h";
+
+// Buat 2 buah LED blink pada pin 12 dan 13
+Blinker led1(12, 100,  400, 10); // (pin, on, off, ulang)
+Blinker led2(13, 3000, 350, 3);  // (pin, on, off, ulang)
+
+void setup() {
+
+}
+
+void loop() {
+    // Jalankan LED
+    led1.Run();
+    led2.Run();
+}
